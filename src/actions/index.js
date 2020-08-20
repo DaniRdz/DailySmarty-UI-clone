c2b8df06acb841b8a94c9256a33689686fb7a088
RdzCore <daniel_rod_con@hotmail.com>
2020-08-20 13:35:33 -0500

    use axios for conect to API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import axios from 'axios';
+
+import {SET_RECENT_POSTS} from './types';
+
 export function fetchRecentPosts() {
     return function (dispatch) {
-        //perform our request in here
-        console.log('hello my friend');
+        axios.get('https://api.dailysmarty.com/posts')
+            .then(response => {
+                console.log('Response', response.data.posts);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts
+                })
+            })
+            .catch(error => {
+                console.log('fetchRecentPost error', error);
+            })
     }
 }
---
+++ src/reducers/post-reducer.js
@@ -0,0 +1,17 @@
+import { SET_RECENT_POST } from '../actions/types';
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function (state = INIT_STATE, action) {
+    switch (action.type) {
+        case SET_RECENT_POST:
+            const recentPosts = action.payload
+            return [...state, recentPosts]
+    
+        default:
+            return state;
+    }
+}
