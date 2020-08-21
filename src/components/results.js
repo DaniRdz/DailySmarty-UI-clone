8c52372e212b3e0e1e73350c8b8b3a84e81f599d
RdzCore <daniel_rod_con@hotmail.com>
2020-08-21 12:13:22 -0500

    render topics

--- src/components/results.js
+++ src/components/results.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Logo from './logo'
+
 export default function () {
     return (
         <div>
+            <Logo />
             <h1>Some Results</h1>
         </div>
     );
